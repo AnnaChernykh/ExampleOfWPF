fa572881f9e6f26e6e1b94f88a29bf62ccaca4f6
Anna Chernykh <annchernykh@gmail.com>
2017-04-25 22:24:23 +0300

    Начальный проект. Элементы интерфеса без логики

---
+++ Example/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example
+{public class param
+        {
+            public string name { get; set; }
+            public double value { get; set; }
+        }
+    
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            //надо, чтоб цеплял и имя, и значение
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+            string s = (selectedItem.Content.ToString());
+            MessageBox.Show("Empty!"+s);
+        }
+
+        
+    }
+}
